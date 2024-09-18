24825d69dcb44a0807396ec70f5d91815304ccc3
HaileySnow <harvey.funda07@gmail.com>
2024-09-18 23:36:33 +0800

    Added Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { NgModule } from '@angular/core';
+export const routes: Routes = [
+{
+    path: "landing", 
+    component: LandingpageComponent
+}, {
+    path: "signup",
+    component: SignupComponent
+}, {
+    path: "login",
+    component: LoginComponent
+}, {
+    path: "home",
+    component: HomepageComponent
+}
+];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
